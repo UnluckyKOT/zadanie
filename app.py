a306d03440f731f3e275b011e1f91c3fe5bac2b4
UnluckyKOT <aalleexx.gf@gmail.com>
2025-05-30 19:12:00 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
-def my_function(  x,y ):
-  print(   "Hello, World!" )
-  return x+y
+def my_function(x, y): 
+    print("Hello, World!")  
+    return x + y           
 
-
-x=1
-y=2
-result=my_function(x ,y )
-print( result )
+x = 1                     
+y = 2                     
+result = my_function(x, y)
+print(result) 
